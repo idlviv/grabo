b3353d52d246ff07b08de4cfc033bf38158e5bb4
idlviv <passpass1>
2018-06-12 14:00:31 +0300

    setup frameworks

--- frontend/src/app/components/products/breadcrumb/breadcrumb.component.ts
+++ frontend/src/app/components/products/breadcrumb/breadcrumb.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { mergeMap } from 'rxjs/operators';
-import { forkJoin } from 'rxjs';
+import { forkJoin, of, merge } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { CatalogService } from '../../../services/catalog.service';
 import { ProductService } from '../../../services/product.service';
@@ -12,6 +12,7 @@ import { ProductService } from '../../../services/product.service';
 })
 export class BreadcrumbComponent implements OnInit {
   category_id: string;
+  categoryName: string;
   currentCategory: any;
   hierarchyCategory: any;
   product_id: string;
@@ -24,42 +25,76 @@ export class BreadcrumbComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.route.paramMap.pipe(
-      mergeMap(paramMap => {
-        this.category_id = paramMap.get('category_id');
-        this.product_id = paramMap.get('product_id');
+    const $paramMap = this.route.paramMap;
+    const $queryParamMap = this.route.queryParamMap;
 
-        // const $getProduct = this.productService.getProductById(this.product_id);
-
-        console.log('bread this.category_id', this.category_id);
-        console.log('bread this.product_id', this.product_id);
-        if (!this.category_id) {
-          return forkJoin(
-            this.catalogService.getAllParents('products'),
-            this.productService.getProductById(this.product_id)
-          );
-          // return this.catalogService.getAllParents('products');
-        }
-        return forkJoin(
-          this.catalogService.getAllParents(this.category_id),
-          this.productService.getProductById(this.product_id)
-        );
-      })
+    merge(
+      $paramMap
+        .pipe(
+        mergeMap(paramMap => {
+          this.category_id = paramMap.get('category_id');
+          console.log('bread this.category_id', this.category_id);
+          if (!this.category_id) {
+            return this.catalogService.getAllParents('products');
+          }
+          return this.catalogService.getAllParents(this.category_id);
+        })
+      )
+      ,
+      $queryParamMap
+      //   .pipe(
+      //   mergeMap(paramMap => {
+      //     this.categoryName = paramMap.get('name');
+      //     console.log('bread this.categoryName', this.categoryName);
+      //     return of(this.categoryName);
+      //   })
+      // )
     )
-        .subscribe(result => {
-          console.log('result bread hierarchy', result[0].data);
-          console.log('result bread product', result[1].data);
-          // result.data[0].hierarchy to splice home => common => mainCategory
-          // this.currentCategory =  result.data[0];
-          this.product = result[1].data;
-          this.hierarchyCategory = result[0].data[0].hierarchy;
-          this.hierarchyCategory.push(result[0].data[0]);
-          this.hierarchyCategory.splice(0, 3);
-          console.log('hierarchyCategory', this.hierarchyCategory);
+      .subscribe(forkData => {
+        console.log('forkData', typeof forkData);
+      });
 
-      },
-          err => console.log('Помилка breadcrumb', err)
-          );
+
+
+
+    ////////////////////////////
+
+    // this.route.paramMap.pipe(
+    //   mergeMap(paramMap => {
+    //     this.category_id = paramMap.get('category_id');
+    //     this.product_id = paramMap.get('product_id');
+    //
+    //     // const $getProduct = this.productService.getProductById(this.product_id);
+    //
+    //     console.log('bread this.category_id', this.category_id);
+    //     console.log('bread this.product_id', this.product_id);
+    //     if (!this.category_id) {
+    //       return forkJoin(
+    //         this.catalogService.getAllParents('products'),
+    //         this.productService.getProductById(this.product_id)
+    //       );
+    //       // return this.catalogService.getAllParents('products');
+    //     }
+    //     return forkJoin(
+    //       this.catalogService.getAllParents(this.category_id),
+    //       this.productService.getProductById(this.product_id)
+    //     );
+    //   })
+    // )
+    //   .subscribe(result => {
+    //       console.log('result bread hierarchy', result[0].data);
+    //       console.log('result bread product', result[1].data);
+    //       // result.data[0].hierarchy to splice home => common => mainCategory
+    //       // this.currentCategory =  result.data[0];
+    //       this.product = result[1].data;
+    //       this.hierarchyCategory = result[0].data[0].hierarchy;
+    //       this.hierarchyCategory.push(result[0].data[0]);
+    //       this.hierarchyCategory.splice(0, 3);
+    //       console.log('hierarchyCategory', this.hierarchyCategory);
+    //
+    //     },
+    //     err => console.log('Помилка breadcrumb', err)
+    //   );
   }
 
 }
