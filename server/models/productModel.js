8df9092123f1e365580abc76dc9eb0770920289a
idlviv <idlviv@rambler.ru>
2018-07-01 18:47:07 +0300

    +

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -15,35 +15,39 @@ const ProductSchema = new Schema({
     type: String,
     required: true,
   },
-  // auth: {
-  //   type: String,
-  //   required: true,
-  // },
-  // order: {
-  //   type: Number
-  // },
-  // prefix: {
-  //   type: String,
-  // },
-  // assets: {
-  //   type: [String],
-  //   default: config.get('defaultProductImgPath'),
-  //   required: true
-  // },
-  // techAssets: {
-  //   type: [String],
-  //   default: config.get('defaultProductImgPath'),
-  //   required: true
-  // },
-  // description: {
-  //   type: String,
-  //   required: true
-  // },
-  // recommendations: {
-  //   type: [String],
-  //   required: true
+  auth: {
+    type: String,
+    required: true,
   },
-);
+  order: {
+    type: Number
+  },
+  prefix: {
+    type: String,
+  },
+  assets: {
+    type: [String],
+    default: config.get('defaultProductImgPath'),
+    required: true
+  },
+  techAssets: {
+    type: [String],
+    default: config.get('defaultProductImgPath'),
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  recommendations: {
+    type: [String],
+    required: true
+  },
+  designs: {
+    type: [String],
+    required: true
+  }
+});
 
 let ProductModel = mongoose.model('products', ProductSchema);
 module.exports = ProductModel;
