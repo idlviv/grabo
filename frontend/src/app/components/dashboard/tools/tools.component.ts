29146137e6d72061d638cdff91b6481b11503fdf
idlviv <idlviv@rambler.ru>
2018-07-25 11:00:41 +0300

    +

--- frontend/src/app/components/dashboard/tools/tools.component.ts
+++ frontend/src/app/components/dashboard/tools/tools.component.ts
@@ -10,12 +10,14 @@ export class ToolsComponent implements OnInit {
   config = config;
   iconFilename: string;
   faviconFilename: string;
+  logoFilename: string;
 
   constructor() { }
 
   ngOnInit() {
     this.iconFilename = 'icon-650x650.png';
     this.faviconFilename = 'favicon.ico';
+    this.logoFilename = 'grabo.png';
   }
 
 }
