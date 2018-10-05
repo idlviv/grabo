21e2665715606f493c73d394929c062e6e3ea3d2
idlviv <idlviv@rambler.ru>
2018-10-05 12:45:38 +0300

    +

--- app.js
+++ app.js
@@ -41,16 +41,6 @@ app.use(logger('dev'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use (function (req, res, next) {
-  if (req.secure) {
-          // request was via https, so do no special handling
-          next();
-  } else {
-          // request was via http, so redirect to https
-          res.redirect('https://' + req.headers.host + req.url);
-  }
-});
-
 /**
  * all apis, api/404 will be handled there
  */
